79717fcc5b4582733570d87389bb8cb7ad3f911d
4lan Lucas <alanlucas0707@gmail.com>
2024-04-26 09:51:28 -0300

    Added Client class and main class features

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import model.Client;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o número da conta: ");
+        int number = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Digite sua agência: ");
+        String agency = sc.nextLine();
+        System.out.print("Digite seu nome: ");
+        String name = sc.nextLine().toUpperCase();
+        System.out.print("Digite o saldo da conta: ");
+        Double balance = sc.nextDouble();
+
+        Client client = new Client(number, agency, name, balance);
+
+        System.out.print("Olá " + client.getClientName());
+        System.out.print(", obrigado por criar uma conta em nosso banco, sua agência é " + client.getAgency());
+        System.out.print(", conta " + client.getNumber());
+        System.out.print(" e seu saldo " + client.getBalance() + " já está disponível para o saque");
+
     }
 }
--- src/model/Client.java
+++ src/model/Client.java
@@ -1,4 +1,61 @@
 package model;
 
 public class Client {
+    private int Number;
+    private String Agency;
+    private String clientName;
+    private double balance;
+
+    public Client(){
+
+    }
+
+    public Client(int number, String agency, String clientName, double balance) {
+        Number = number;
+        Agency = agency;
+        this.clientName = clientName;
+        this.balance = balance;
+    }
+
+    public int getNumber() {
+        return Number;
+    }
+
+    public void setNumber(int number) {
+        Number = number;
+    }
+
+    public String getAgency() {
+        return Agency;
+    }
+
+    public void setAgency(String agency) {
+        Agency = agency;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "Number=" + Number +
+                ", Agency='" + Agency + '\'' +
+                ", clientName='" + clientName + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
